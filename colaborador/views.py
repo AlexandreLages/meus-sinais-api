9b4ee233b636a5fc18b17a428dbe20f79271d21e
Otavio Cury <otaviocury.oc@gmail.com>
2020-06-25 23:56:58 -0300

    Melhorando o endpoint de cadastro de colaborador,

--- colaborador/views.py
+++ colaborador/views.py
@@ -1,14 +1,20 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+from core.models import Colaborador
+
 
 class CadastroColaboradorView(APIView):
 
+	@api_view(['POST'])
 	def post(self, request):
 		usuario = request.data['usuario']
 		senha = request.data['senha']
-		confirma_senha = request.data['confirmarSenha']
 		email = request.data['email']
 		nome = request.data['nome']
 		telefone = request.data['telefone']
@@ -18,9 +24,36 @@ class CadastroColaboradorView(APIView):
 		crp = request.data['crp']
 		perfil = request.data['perfil']
 
-		user = User.objects.filter(username=usuario)
+		user_username = User.objects.filter(username=usuario).first()
+		user_cpf = User.objects.filter(cpf=cpf).first()
+		colaborador_email = Colaborador.objects.filter(email=email).first()
+		colaborador_crp = Colaborador.objects.filter(crp=crp).first()
 
-		if not user:
+		if user_username is not None:
+			response = {
+				"status": 409,
+				"message": "Já existe um usuário com o mesmo Usuário!"
+			}
+			return Response({"response": response})
+		elif user_cpf is not None:
+			response = {
+				"status": 409,
+				"message": "Já existe um usuário com o mesmo CPF!"
+			}
+			return Response({"response": response})
+		elif colaborador_email is not None:
+			response = {
+				"status": 409,
+				"message": "Já existe um usuário com o mesmo Email!"
+			}
+			return Response({"response": response})
+		elif colaborador_crp is not None:
+			response = {
+				"status": 409,
+				"message": "Já existe um usuário com o mesmo CRP!"
+			}
+			return Response({"response": response})
+		else:
 			user = User.objects.create_user(usuario, email, senha)
 			user.save()
 
@@ -30,23 +63,13 @@ class CadastroColaboradorView(APIView):
 				telefone_fixo=telefone,
 				cpf=cpf,
 				rg=rg, email=email,
-				crp=crp, perfil=perfil,
-				foto=foto
+				crp=crp, perfil=perfil
 			)
-
 			user = User.objects.get(username=usuario)
 			colaborador.user = user
 			colaborador.save()
-		else:
 			response = {
-				"status": 409,
-				"message": "Já existe um usuário com dados conflitantes!" 
+				"status": 200,
+				"message": "Usuário cadastrado com sucesso!"
 			}
 			return Response({"response": response})
-
-		response = {
-			"status": 200,
-			"message": "Usuário cadastrado com sucesso!" 
-		}
-
-		return Response({"response": responses})
