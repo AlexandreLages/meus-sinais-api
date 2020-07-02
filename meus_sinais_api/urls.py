75aead36dfa429572a85400cfc3d21b8759935ca
Alexandre Lages <alexandrelaages@gmail.com>
2020-07-01 22:06:32 -0300

    Criando o get-token para login

--- meus_sinais_api/urls.py
+++ meus_sinais_api/urls.py
@@ -16,10 +16,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('api/colaborador/', include('colaborador.urls')),
     path('api/paciente/', include('paciente.urls')),
+    path('get-token/', obtain_jwt_token),
+    path('refresh-token/', refresh_jwt_token),
 ]
